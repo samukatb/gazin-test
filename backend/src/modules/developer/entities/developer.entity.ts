1a59a3036908682e5ef1b7405a04d8ee072be7e3
Samuel Kersbaumer <samukatb2010@hotmail.com>
2023-02-28 20:16:39 -0300

    feat: developer entity

--- backend/src/modules/developer/entities/developer.entity.ts
+++ backend/src/modules/developer/entities/developer.entity.ts
@@ -1,1 +1,29 @@
-export class Developer {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { SexType } from '../enums/sex.enum';
+
+@Entity('developers')
+export class Developer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: SexType,
+  })
+  sex: SexType;
+
+  @Column()
+  birthdate: Date;
+
+  @Column()
+  hobby: string;
+
+  get age(): number {
+    const diffInMs = Date.now() - this.birthdate.getTime();
+    const ageDate = new Date(diffInMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+}
